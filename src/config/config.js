fc902d82c9eaebd91d629a884f98ccf77dfb79e8
Rodrigo Mattioda <rodrigo-mattioda@hotmail.com>
2020-08-14 17:09:31 -0300

    correçao fake backend

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@
 //   ? 'http://localhost:8080'
 //   : 'https://todos.herokuapp.com';
 
-const URL_BACKEND = 'https://to-do-theta.vercel.app/';
+const URL_BACKEND = window.location.hostname;
 
 export default {
   URL_BACKEND,
