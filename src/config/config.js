06d861d9a0f0b98f1037fb9b7d695f290bd12a26
Rodrigo Mattioda <rodrigo-mattioda@hotmail.com>
2020-08-14 17:28:42 -0300

    correçao backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://two-dou.herokuapp.com/';
+  : 'https://two-dou.herokuapp.com';
 
 export default {
   URL_BACKEND,
