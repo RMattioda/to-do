d4d13f079673895f5e6c8b36e5271a5fecb9127d
Rodrigo Mattioda <rodrigo-mattioda@hotmail.com>
2020-08-14 17:07:37 -0300

    adicionando fake backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
-const URL_BACKEND = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'https://todos.herokuapp.com';
+// const URL_BACKEND = window.location.hostname.includes('localhost')
+//   ? 'http://localhost:8080'
+//   : 'https://todos.herokuapp.com';
+
+const URL_BACKEND = 'https://to-do-theta.vercel.app/';
 
 export default {
   URL_BACKEND,
